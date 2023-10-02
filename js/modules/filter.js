30bf4edd75e399a301d39a3617a0aa37a3cbd86e
Simon Karas-Jensen <simo754p@elev.tec.dk>
2023-10-02 11:07:55 +0200

    Add project files.

---
+++ js/modules/filter.js
@@ -0,0 +1,20 @@
+
+const IGNORE = [
+    "films",
+    "vehicles",
+    "starships",
+    "homeworld",
+    "created",
+    "edited",
+    "url",
+    "species",
+    "residents",
+    "planets",
+    "characters",
+    "people",
+    "pilots"
+]
+
+export {
+    IGNORE
+}
